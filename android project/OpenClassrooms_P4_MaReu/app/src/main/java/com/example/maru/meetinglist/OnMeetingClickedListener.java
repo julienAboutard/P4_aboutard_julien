7a1dbbfb7945ae40e2c92cf9e7a766cae43c853e
Julien <julien.aboutard@gmail.com>
2023-10-27 14:17:10 +0200

    End of repository fixing

--- android project/OpenClassrooms_P4_MaReu/app/src/main/java/com/example/maru/meetinglist/OnMeetingClickedListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.maru.meetinglist;
-
-public interface OnMeetingClickedListener {
-    void onMeetingClicked(long neighbourId);
-
-    void onDeleteMeetingClicked(long neighbourId);
-}
